082589d5c84b8fe2b3693dfd8630bb0ff7c60c6b
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-12 21:43:38 -0300

    Criação das entidades.

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,6 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+import { Tables } from './../database'
+
+@Entity(Tables.Categories)
 export default class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column()
   title: string
+
+  @CreateDateColumn()
   created_at: Date
+
+  @UpdateDateColumn()
   updated_at: Date
 }
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,9 +1,45 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm'
+
+import { Tables } from './../database'
+import Category from './Category'
+
+export enum TransactionType {
+  Income = 'income',
+  Outcome = 'outcome',
+}
+
+@Entity(Tables.Transactions)
 export default class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column()
   title: string
-  type: 'income' | 'outcome'
+
+  @Column()
+  type: TransactionType
+
+  @Column()
   value: number
+
+  @Column()
   category_id: string
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category
+
+  @CreateDateColumn()
   created_at: Date
+
+  @UpdateDateColumn()
   updated_at: Date
 }
